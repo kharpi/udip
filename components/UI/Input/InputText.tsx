33d6bae2a0c28c5bb7bcd601590e54244f3aed65
Kárpáti Tamás <tamas.nandor.karpati@gmail.com>
2022-02-08 14:10:31 +0100

    changed inputtext

--- components/UI/Input/InputText.tsx
+++ components/UI/Input/InputText.tsx
@@ -1,67 +1,38 @@
 import React from 'react';
+import { IInputText } from '../../../interfaces/Input.interface';
+import { IInputChange } from '../../../interfaces/InputChange.interface';
 
-type Props = {
-	classes?: string[];
-	id: string;
-	name?: string;
-	type: 'text' | 'email' | 'password' | 'tel' | 'number' | 'url';
-	defaultValue?: string | number;
-	value?: string;
-	disabled?: boolean;
-	required?: boolean;
-	placeholder?: string;
-	label?: string;
-	onChange?: any;
-	withoutWrap?: boolean;
+type ITextInputProps = Omit<IInputText, 'type'> & {
+	onChange: (e: IInputChange) => void;
+	type?: string;
+	style?: React.CSSProperties;
 };
 
-const InputText = ({
-	classes,
-	id,
-	name,
-	type,
-	defaultValue,
-	value,
-	disabled,
-	required,
-	placeholder,
-	label,
-	onChange,
-	withoutWrap,
-}: Props) => {
-	let wrapperClass = 'input__wrapper';
-	if (classes) wrapperClass += ` ${classes.join(' ')}`;
+const InputText = (props: ITextInputProps): React.ReactElement => {
+	const wrapperStyle: string[] = ['input__wrapper'];
+
+	if (props.hidden) wrapperStyle.push('hidden');
+	if (props.className) wrapperStyle.push(props.className);
+
 	return (
-		<>
-			{!withoutWrap ? (
-				<div className={wrapperClass}>
-					<label>{label}</label>
-					<input
-						id={id}
-						name={name}
-						type={type}
-						defaultValue={defaultValue}
-						value={value}
-						disabled={disabled}
-						required={required}
-						placeholder={placeholder}
-						onChange={onChange}
-					/>
-				</div>
-			) : (
-				<input
-					id={id}
-					name={name}
-					type={type}
-					defaultValue={defaultValue}
-					value={value}
-					disabled={disabled}
-					required={required}
-					placeholder={placeholder}
-					onChange={onChange}
-				/>
-			)}
-		</>
+		<div className={wrapperStyle.join(' ')} style={props.style}>
+			<label htmlFor={props.id}>{props.labelText}</label>
+			<input
+				type={props.type}
+				id={props.id}
+				value={props.value}
+				required={props.required}
+				disabled={props.disabled}
+				pattern={props.pattern}
+				placeholder={props.placeholder}
+				onChange={(e) => {
+					props.onChange({
+						fieldName: props.id,
+						value: e.target.value,
+					});
+				}}
+			/>
+		</div>
 	);
 };
 
